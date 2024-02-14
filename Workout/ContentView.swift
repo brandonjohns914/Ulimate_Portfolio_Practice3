a7fad5a0ae730832a082182093ecbe078550bdc9
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2024-02-14 13:29:08 -0800

    first commit

---
+++ Workout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Workout
+//
+//  Created by Brandon Johns on 2/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Workout/WorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutApp.swift
+//  Workout
+//
+//  Created by Brandon Johns on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
